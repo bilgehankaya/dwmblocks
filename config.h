b534d9525eecd4d5662035ce24f55eca9a07fda3
Bilgehan Kaya <bilbat35@gmail.com>
2020-08-27 17:40:51 +0300

    Some adjustment

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"",	"music",	0,	11},*/
-	{"",	"dummy",	0,	50}, // dummy index
+	{"",	"dummy",	0,	50}, // dummy block
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11},*/
 	/*{"",	"pacpackages",	0,	8},*/
 	/*{"",	"news",		0,	6},*/
+	/* {"", 	"net_speed", 	1,	21}, */
 	{"",	"torrent",	20,	7},
 	{"", 	"vpn --status", 20,	19},
 	{"",	"crypto btc",	0,	13},
 	{"",	"crypto eth",	0,	13},
-	/* {"",	"net_speed",	1,	21}, */
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
@@ -19,7 +19,8 @@ static const Block blocks[] = {
 	{"", 	"memory",	3,	14},
 	{"",	"battery",	10,	3},
 	{"",	"date_time",	10,	1},
-	{"",	"internet",	10,	4},
+	/* {"",	"internet",	10,	4}, */
+	{"",	"keyboard -s",	0,	25},
 	/* {" ",	"kbselect", 	0,	21}, */
 };
 
