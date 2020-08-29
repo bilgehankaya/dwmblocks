a09324366a9e4984ad345ae87b66c05fb8fd18b3
Bilgehan Kaya <bilbat35@gmail.com>
2020-08-29 20:53:18 +0300

    Some adjustments.

--- config.h
+++ config.h
@@ -1,24 +1,18 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"",	"music",	0,	11},*/
 	{"",	"dummy",	0,	50}, // dummy block
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11},*/
-	/*{"",	"pacpackages",	0,	8},*/
-	/*{"",	"news",		0,	6},*/
+	{"", 	"cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"music",	0,	11},
 	/* {"", 	"net_speed", 	1,	21}, */
 	{"",	"torrent",	20,	7},
 	{"", 	"vpn --status", 20,	19},
 	{"",	"crypto btc",	0,	13},
 	{"",	"crypto eth",	0,	13},
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	/* {"",	"weather",	18000,	5}, */
 	{"",	"volume",	0,	10},
 	{"", 	"memory",	3,	14},
 	{"",	"battery",	10,	3},
-	{"",	"date_time",	10,	1},
+	{"",	"date_time -d",	10,	1},
+	{"",	"date_time -c",	10,	1},
 	/* {"",	"internet",	10,	4}, */
 	{"",	"keyboard -s",	0,	25},
 	/* {" ",	"kbselect", 	0,	21}, */
