bb069acb9c89af28382c80bf6573942d50c0eb00
Bilgehan Kaya <bilbat35@gmail.com>
2020-09-07 00:45:00 +0300

    Some blocks are updated.

--- config.h
+++ config.h
@@ -3,12 +3,11 @@ static const Block blocks[] = {
 	{"",	"dummy",	0,	50}, // dummy block
 	{"", 	"cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"", 	"crypto cur", 	0,	13},
-	{"", 	"net_speed", 	1,	21},
+	{"",    "net_speed", 	1,	21},
 	{"",	"torrent",	20,	7},
 	{"", 	"vpn --status", 20,	19},
-	{"",	"crypto btc",	0,	13},
-	{"",	"crypto eth",	0,	13},
+	/* {"",	"crypto btc",	0,	13}, */
+	/* {"",	"crypto eth",	0,	13}, */
 	{"",	"volume",	0,	10},
 	{"", 	"memory",	1,	14},
 	{"",	"battery",	10,	3},
@@ -19,7 +18,7 @@ static const Block blocks[] = {
 };
 
 /* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
-static char *delim = " | ";
+static char *delim = " / ";
 
 /* vim with the following line in your vimrc/init.vim: */
 /* autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & } */
